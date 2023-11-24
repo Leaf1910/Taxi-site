13101b1fcfead5d4d61b5657db18451b55dd79e1
Daksh Prabhulal Dholu <95246067+Leaf1910@users.noreply.github.com>
2023-11-23 22:04:31 -0500

    index_login.php

--- index_login.php
+++ index_login.php
@@ -21,28 +21,28 @@
                 $con = mysqli_connect('127.0.0.1','root','');
                 mysqli_select_db($con, 'taxi_service');
                 if (isset($_SESSION['customer_id'])) {
-                    // Get the customer's name from the session
+                    
                     $customerid = $_SESSION['customer_id'];
                 
-                    // Query to get the customer's name
+                 
                     $n = "SELECT customer_name FROM customers_table WHERE customer_id='$customerid'";
                     $nameResult = mysqli_query($con, $n);
                 
                     if ($nameResult) {
-                        // Fetch the result as an associative array
+                        
                         $row = mysqli_fetch_assoc($nameResult);
                 
-                        // Get the customer name
+                       
                         $customerName = $row['customer_name'];
-                
-                        // Display a welcome message
-                        echo '<span class="welcome-message">Welcome, <span class="user-name">' . $customerName . '</span>!</span>';
+                        
+                        echo '<a><span class="welcome-message">Welcome, <span class="user-name">' . $customerName . '</span>!</span></a>';
                     } else {
-                        // Handle the error if the query fails
+                       
                         echo 'Error: ' . mysqli_error($con);
                     }
                 }
                 ?>
+
                 <a href="index.php">Log Out</a>
                 
             </nav>
@@ -53,14 +53,12 @@
         </div>
     </div>
 
-<!-- home section -->
 <div class="home-container">
     <div class="home-content">
         <div class="inner-content">
-            <h3>best in city</h3>
-            <h2>trusted cab sservice in county</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum nihil deserunt odio repellendus dolorum suscipit excepturi placeat facilis dignissimos officiis aut nam, delectus possimus rerum consequatur similique! Deserunt, vitae. Officiis?</p>
-            <a href="#" class="review">Leave a review</a>
+            <h3>Best in City</h3>
+            <h2>Trusted cab Service in County</h2>
+
         </div>
         <div class="inner-content">
            <div class="contact-form">
@@ -83,123 +81,157 @@
         </div>
     </div>
 </div>
-<!-- home section ended -->
 
-<!-- our tariff -->
 
 <div class="main-tariff">
-    <h1>our <span>tarrif</span></h1>
-    <div class="inner-tarrif">
-        <div class="tarrif-container">
-            <div class="inner-box">
-                <img src="images/image1.png" alt="">
-                <h2>economy class</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat illum officia fugiat, autem facere iste repellendus omnis nemo dolore</p>
-                <h3>price: $3 /-</h3>
-                <a href="#">order now</a>
-            </div>
-        </div>
+        <h1>
+            MORE WAYS TO RIDE WITH US
+            
+        </h1>
+        <div class="inner-tarrif">
+            <div class="tarrif-container">
+                <div class="inner-box">
+                    <img src="https://becktaxi.com/cdn/shop/t/6/assets/beck-chits_.png?v=84708642258601155431613419179" alt="">
+                    <h2>CABHUB BUCKS</h2>
+                    <p>
+                        Think about Cabhub Bucks as a distinctive present idea! They're a fantastic method to enjoy riding in freedom with Cabhub. They make excellent gifts for employees, Christmas stuffers, and handy backups at home!
 
-        <div class="tarrif-container">
-            <div class="inner-box">
-                <img src="images/image1.png" alt="">
-                <h2 class="heading-yellow">economy class</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat illum officia fugiat, autem facere iste repellendus omnis nemo dolore</p>
-                <h3 class="yellw-section">price: $3 /-</h3>
-                <a href="#" class="btn-yellow">order now</a>
-            </div>
-        </div>
+                    </p>
 
-        <div class="tarrif-container">
-            <div class="inner-box">
-                <img src="images/image1.png" alt="">
-                <h2>economy class</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat illum officia fugiat, autem facere iste repellendus omnis nemo dolore</p>
-                <h3>price: $3 /-</h3>
-                <a href="#">order now</a>
+
+                </div>
+            </div>
+
+            <div class="tarrif-container">
+                <div class="inner-box">
+                    <img src="https://becktaxi.com/cdn/shop/t/6/assets/beck-events_.png?v=181923313640530627661613419180" alt="">
+                    <h2 class="heading-yellow">
+                        Special Events
+                    </h2>
+                    <p>
+                        If you are planning an event, large or small, we can help with all your transportation requirements. We can work with you to create a custom experience for your guests to arrive and get home safe.
+
+                    </p>
+                    <h3 class="yellw-section"> </h3>
+
+                </div>
+            </div>
+
+            <div class="tarrif-container">
+                <div class="inner-box">
+                    <img src="https://becktaxi.com/cdn/shop/t/6/assets/beck-accounts_.png?v=18679035097341112701613419179" alt="">
+                    <h2>
+                        Accounts
+                    </h2>
+                    <p>
+                        Create a CabHub monthly charge account to fit your travel needs.
+
+                    </p>
+                    <h3> </h3>
+
+                </div>
             </div>
         </div>
     </div>
-</div>
 
 
-<!-- our tariff ended -->
+  
+    <div class="fast-booking">
+        <h1 class="fast-hading">we do best</h1>
+        <h2>than you wish</h2>
+        <div class="inner-fast">
+            <div class="booking-content">
+                <div class="icon-fast">
+                    <span><i class="fas fa-star"></i></span>
+                </div>
+                <div class="inner-fast-text">
+                    <h1>Automated Fare Calculation</h1>
+                    <p>Implement a reliable fare calculation system based on factors such as distance, time, and any additional charges. Provide fare estimates before confirming the booking</p>
 
-<!-- fast booking -->
-<div class="fast-booking">
-    <h1 class="fast-hading">we do best</h1>
-    <h2>than you wish</h2>
-    <div class="inner-fast">
-        <div class="booking-content">
-            <div class="icon-fast">
-                <span><i class="fas fa-star"></i></span>
+                </div>
+
             </div>
-            <div class="inner-fast-text">
-                <h1>fast booking</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae debitis asperiores adipisci, totam volu</p>
-            
-            </div>
-                
-        </div>
-        <div class="booking-content">
-            <div class="icon-fast">
-                <span><i class="fas fa-map-marker-alt"></i></span>
-            </div>
-            <div class="inner-fast-text">
-                <h1>fast booking</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae debitis asperiores adipisci, totam volu</p>
-            
-            </div>
-        </div>
-        <div class="booking-content">
-            <div class="icon-fast">
-                <span><i class="fas fa-map-marker-alt"></i></span>
-            </div>
-            <div class="inner-fast-text">
-                <h1>fast booking</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae debitis asperiores adipisci, totam volu</p>
-            
-            </div>
-        </div>
-        <div class="booking-content">
-            <div class="icon-fast">
-                <span><i class="fas fa-map-marker-alt"></i></span>
-            </div>
-            <div class="inner-fast-text">
-                <h1>fast booking</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae debitis asperiores adipisci, totam volu</p>
-            
-            </div>
-        </div>
-    </div>
-</div>
+            <div class="booking-content">
+                <div class="icon-fast">
+                    <span><i class="fas fa-map-marker-alt"></i></span>
+                </div>
+                <div class="inner-fast-text">
+                    <h1>Real time tracking</h1>
+                    <p>
+                        Implement real-time tracking of the booked taxi. Passengers should be able to track the location of the approaching vehicle, providing transparency and reducing anxiety about arrival times.
+                    </p>
 
-
-
-
-<!-- fast booking end -->
-
-
-
-<div class="testimonials">
-    <h1 class="heading-test">happy clients</h1>
-    <div class="main-testimonials">
-        <div class="inner-test">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe veritatis fugit magnam esse, amet adipisci eaque dolore explicabo quidem laudantium ad, enim, obcaecati optio? Harum porro delectus accusamus assumenda ullam.</p>
-            <div class="clients">
-                <img src="images/jhon.png" alt="">
-                <h1>jhone doe</h1>
+                </div>
             </div>
-        </div>
-        <div class="inner-test">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe veritatis fugit magnam esse, amet adipisci eaque dolore explicabo quidem laudantium ad, enim, obcaecati optio? Harum porro delectus accusamus assumenda ullam.</p>
-            <div class="clients">
-                <img src="images/jhon.png" alt="">
-                <h1>jhone doe</h1>
+            <div class="booking-content">
+                <div class="icon-fast">
+                    <span><i class="fas fa-map-marker-alt"></i></span>
+                </div>
+                <div class="inner-fast-text">
+                    <h1>Secure Payment Options</h1>
+                    <p>Offer secure and convenient payment options. Support various payment methods, including credit/debit cards, mobile wallets, and in-app payments</p>
+
+                </div>
+            </div>
+            <div class="booking-content">
+                <div class="icon-fast">
+                    <span><i class="fas fa-map-marker-alt"></i></span>
+                </div>
+                <div class="inner-fast-text">
+                    <h1>Rating and Reviews</h1>
+                    <p>
+                        Allow passengers to rate and review drivers, and vice versa. This helps maintain service quality and builds trust within the community.
+                    </p>
+
+                </div>
             </div>
         </div>
     </div>
-</div>
+
+
+
+
+
+
+
+
+    <div class="testimonials">
+        <h1 class="heading-test">happy clients</h1>
+        <div class="main-testimonials">
+            <div class="inner-test">
+                <p>
+                    "Wow, I can't say enough good things about this taxi service! The drivers are consistently friendly and punctual. I've used their service multiple times, and each experience has been fantastic. The cars are always clean and comfortable, making my rides enjoyable. Booking is a breeze, and the customer service is top-notch. I highly recommend this taxi service to anyone looking for a reliable and pleasant transportation experience!"
+                    .
+                </p>
+                <div class="clients">
+                    <img src="images/jhon.png" alt="images/jhon" width="150" height="200">
+                    <h1>jhone doe</h1>
+                </div>
+            </div>
+            <div class="inner-test">
+                <p>
+                    "I am beyond impressed with the exceptional service provided by this taxi company. The drivers are not only skilled and efficient but also incredibly courteous. I've never encountered a more reliable transportation service. The ease of booking through their user-friendly app is a game-changer. The vehicles are always in pristine condition, and the overall experience is consistently delightful. I wholeheartedly recommend this taxi service to everyone – a perfect blend of professionalism and comfort!"
+                    .
+                </p>
+                <div class="clients">
+                    <img src="https://i.ytimg.com/vi/ZlcaOw5egA0/maxresdefault.jpg" alt="https://i.ytimg.com/vi/ZlcaOw5egA0/maxresdefault" width="150" height="150">
+                    <h1>Sana Rathod</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     <script src="script.js"></script>
